c5fb6e7a8bce85c2400ef825563b179af6b211c4
sam <sbenitez.js@gmail.com>
2024-10-08 00:12:57 -0300

    feat: password value object

---
+++ src/core/domain/user/value-objects/Password.ts
@@ -0,0 +1,38 @@
+
+export class Password 
+{
+    public static MIN_LENGTH = 8
+    public static MAX_LENGTH = 24
+
+    constructor(public readonly value: string) {
+        this.throwErrorIfisEmpty(value)
+        this.throwErrorIfLengthIsLessThanMinimumLength(value)
+        this.throwErrorIfLengthIsGreatherThanMaximumLength(value)
+        this.throwErrorIfValueIsInvalidFormat(value)
+    }
+
+    private throwErrorIfisEmpty (value: string): void {
+        if (value.length === 0) {
+          throw new Error()
+        }
+    }
+
+    private throwErrorIfLengthIsLessThanMinimumLength(value: string): void {
+        if(value.length < Password.MIN_LENGTH) {
+            throw new Error()
+        }
+    }
+
+    private throwErrorIfLengthIsGreatherThanMaximumLength(value: string): void {
+        if(value.length > Password.MAX_LENGTH) {
+            throw new Error()
+        }
+    }
+
+    private throwErrorIfValueIsInvalidFormat(value: string): void {
+        const regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).+$/
+        if(regex.test(value) === false) {
+            throw new Error()
+        }
+    }
+}
---
+++ src/test/core/domain/user/value-objects/password.test.ts
@@ -0,0 +1,26 @@
+import { Password } from "../../../../../core/domain/user/value-objects/Password"
+
+describe('Password Value Object', () => {
+    it('should to be defined if valid password', () => {
+        expect(() => {
+            new Password('@123Abcd')
+        }).not.toThrow()
+    })
+
+
+    it('should throw error if password is empty', () => {
+        expect(() => new Password('')).toThrow()
+    })
+
+    it('should throw error if password is not valid format', () => {
+        expect(() => new Password('a'.repeat(Password.MIN_LENGTH - 1))).toThrow()
+        expect(() => new Password('a'.repeat(Password.MAX_LENGTH + 1))).toThrow()
+        expect(() => new Password('12345678')).toThrow()
+        expect(() => new Password('abcdefgh')).toThrow()
+        expect(() => new Password('1234abcd')).toThrow()
+        expect(() => new Password('1234@abcd')).toThrow()
+        expect(() => new Password('1234Aabcd5')).toThrow()
+        expect(() => new Password('123@#/=?')).toThrow()
+        expect(() => new Password('ABC@#/=?')).toThrow()
+      })
+})
